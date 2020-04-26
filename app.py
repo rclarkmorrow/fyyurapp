6777437d8c3f1299aa8938ee5021b019930f0966
Rick Morrow <rclark.morrow@gmail.com>
2020-04-25 22:16:57 -0700

    Database models in models.py
    Created a separate file for database models in models.py, updated
    app.py to import models.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 """--------------------------------------------------------------------------#
 # Imports
 #--------------------------------------------------------------------------"""
+
+
 import sys
 import json
 import dateutil.parser
@@ -14,6 +16,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from models import db, Venue, Artist
+
 
 """--------------------------------------------------------------------------#
 # App Config.
@@ -22,54 +26,8 @@ from forms import *
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
-
-
-"""--------------------------------------------------------------------------#
-# Models.
-#--------------------------------------------------------------------------"""
-
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    defaultImg = "https://placebear.com/400/400"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False, unique=True)
-    genres = db.Column(db.String)
-    address = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, nullable=False, server_default='f')
-    seeking_description = db.Column(db.String(500))
-    image_link = db.Column(db.String(500), nullable=False, default=defaultImg)
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    defaultImg = "https://placekitten.com/2000/2000"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False, unique=True)
-    genres = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=False, server_default='f')
-    seeking_description = db.Column(db.String(500))
-    image_link = db.Column(db.String(500), nullable=False, default=defaultImg)
-
-
-# TODO Implement Show and Artist models, and complete all model
-# relationships and properties, as a database migration.
 
 
 """--------------------------------------------------------------------------#
---
+++ models.py
@@ -0,0 +1,55 @@
+"""--------------------------------------------------------------------------#
+# Imports
+#--------------------------------------------------------------------------"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+"""--------------------------------------------------------------------------#
+# Models
+#--------------------------------------------------------------------------"""
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    defaultImg = "https://placebear.com/400/400"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    genres = db.Column(db.String)
+    address = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, server_default='f')
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500), nullable=False, default=defaultImg)
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    defaultImg = "https://placekitten.com/2000/2000"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    genres = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, server_default='f')
+    seeking_description = db.Column(db.String(500))
+    image_link = db.Column(db.String(500), nullable=False, default=defaultImg)
+
+
+# TODO Implement Show and Artist models, and complete all model
+# relationships and properties, as a database migration.
+
+
